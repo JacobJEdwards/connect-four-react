80133d0d4c727164786eeee2aa91b3f1aa1b1e9e
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-06 12:05:18 +0100

    game board functions

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,35 +1,51 @@
 import React, { useState } from 'react'
 import './Board.css'
 
-enum pieceColour {
-    RED,
-    BLUE
-}
 
-function Piece() {
+function Piece(props: any) {
     return (
-        // todo
+        <span className={props.colour + ' piece'}></span>
     )
 }
-
 function Board() {
-    const [grid, setGrid] = useState<any[]>(
-        new Array(7).fill(
-            new Array(6).fill(0)
+    const [grid, setGrid] = useState<number[][]>(
+        new Array(6).fill(
+            new Array(7).fill(0)
         )
     );
 
     const [turn, setTurn] = useState<number>(1);
 
     function handleClick(index: number) {
-        const colour: pieceColour = turn % 2 === 0 ? pieceColour.BLUE : pieceColour.RED;
+        const player: number = turn % 2 === 0 ? 1 : 2;
 
+        const gridCopy = [...grid];
+        const columnCopy = [...grid[index]].reverse();
+
+        columnCopy.some((pieceNumber, index) => {
+            if (pieceNumber === 0) {
+                columnCopy[index] = player;
+                return true;
+            }
+        })
+
+        gridCopy[index] = columnCopy.reverse();
+        setGrid(gridCopy);
+        setTurn(turn + 1);
     }
 
     return (
         <div className='board'>
-            {grid.map((piece, index) => (
-                <div className={`slot ${piece}`} key={index} onClick={() => handleClick(index)}>piece</div>
+            {grid.map((column, index) => (
+                <div className='column' onClick={() => handleClick(index)}>
+                    {column.map(piece => (
+                        <Piece colour={
+                            piece == 0 ? 'black'
+                                : piece == 1 ? 'red'
+                                : 'green'
+                        } />
+                    ))}
+                </div>
             ))}
         </div>
     )
