870c55f171f99962ad309b91b810a2bc3d4a63ee
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-07 13:17:03 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Board from './Board'
 const App = (): ReactElement => {
   return (
     <main className='App'>
-      <h1>Connect Four</h1>
       <Board
         numRows={6}
         numCols={7}
