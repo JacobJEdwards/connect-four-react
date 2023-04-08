4a3872c60031e236a268f36a730a90db6c062c32
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-08 12:05:22 +0100

    refactored win check

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
 import React, { useState, useRef, memo } from 'react'
 import type { FC } from 'react'
 import './Board.css'
@@ -25,6 +26,76 @@ interface ColumnProps {
   column: BoardPiece[]
   won: boolean
   onClick: () => void
+}
+
+interface Direction {
+  dx: number
+  dy: number
+}
+
+function checkWin(grid: BoardPiece[][], player: BoardPiece): boolean {
+  const directions: Direction[] = [
+    { dx: 1, dy: 0 },
+    { dx: 0, dy: 1 },
+    { dx: 1, dy: 1 },
+    { dx: 1, dy: -1 },
+  ]
+
+  const checkLine = (line: BoardPiece[], player: BoardPiece): boolean => {
+    let count = 0
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] === player) {
+        count++
+        if (count === WINNING_PIECES) {
+          return true
+        }
+      } else {
+        count = 0
+      }
+    }
+    return false
+  }
+
+  const checkDirection = (
+    grid: BoardPiece[][],
+    player: BoardPiece,
+    startCol: number,
+    startRow: number,
+    dir: Direction
+  ): boolean => {
+    const { dx, dy } = dir
+    const endCol = startCol + dx * (WINNING_PIECES - 1)
+    const endRow = startRow + dy * (WINNING_PIECES - 1)
+    if (
+      endCol < 0 ||
+      endCol >= grid[0].length ||
+      endRow < 0 ||
+      endRow >= grid.length
+    ) {
+      return false
+    }
+    const line: BoardPiece[] = []
+    for (let i = 0; i < WINNING_PIECES; i++) {
+      const col = startCol + dx * i
+      const row = startRow + dy * i
+      line.push(grid[row][col])
+    }
+    return checkLine(line, player)
+  }
+
+  for (let col = 0; col < grid[0].length; col++) {
+    for (let row = 0; row < grid.length; row++) {
+      if (grid[row][col] !== player) {
+        continue
+      }
+      for (const dir of directions) {
+        if (checkDirection(grid, player, col, row, dir)) {
+          return true
+        }
+      }
+    }
+  }
+  return false
 }
 
 // "Board Piece" component -> takes a colour prop and returns a span with that colour
@@ -70,77 +141,6 @@ const Board: FC<BoardProps> = ({ numRows = 6, numCols = 7 }: BoardProps) => {
     setGameWon(false)
   }
 
-  const checkWin = (
-    newGrid: BoardPiece[][],
-    colIndex: number,
-    rowIndex: number,
-    player: BoardPiece
-  ): boolean => {
-    const checkLine = (line: BoardPiece[]): boolean => {
-      let count = 0
-      for (let i = 0; i < line.length; i++) {
-        if (line[i] === player) {
-          count++
-          if (count === WINNING_PIECES) {
-            return true
-          }
-        } else {
-          count = 0
-        }
-      }
-      return false
-    }
-
-    // check column
-    if (checkLine(newGrid[colIndex])) {
-      return true
-    }
-
-    // check row
-    const row = newGrid.map((column) => column[rowIndex])
-    if (checkLine(row)) {
-      return true
-    }
-
-    // check diagonal (top-left to bottom-right)
-    let rowStart = rowIndex
-    let colStart = colIndex
-    while (rowStart > 0 && colStart > 0) {
-      rowStart--
-      colStart--
-    }
-
-    let diagonal: BoardPiece[] = []
-    while (rowStart < numRows && colStart < numCols) {
-      diagonal.push(newGrid[rowStart][colStart])
-      rowStart++
-      colStart++
-    }
-    if (checkLine(diagonal)) {
-      return true
-    }
-
-    // check diagonal (top-right to bottom-left)
-    rowStart = rowIndex
-    colStart = colIndex
-    while (rowStart > 0 && colStart < numCols - 1) {
-      rowStart--
-      colStart++
-    }
-
-    diagonal = []
-    while (rowStart < numRows && colStart >= 0) {
-      diagonal.push(newGrid[rowStart][colStart])
-      rowStart++
-      colStart--
-    }
-    if (checkLine(diagonal)) {
-      return true
-    }
-
-    return false
-  }
-
   // handle click function -> takes an index, which is the column that was clicked. It then checks if the column is full, and if it isn't, it adds a piece to the bottom of the column
   const handleClick = (colIndex: number): void => {
     if (gameWon) {
@@ -157,7 +157,7 @@ const Board: FC<BoardProps> = ({ numRows = 6, numCols = 7 }: BoardProps) => {
         setGrid(newGrid)
         turn.current++
 
-        if (checkWin(newGrid, colIndex, rowIndex, player)) {
+        if (checkWin(newGrid, player)) {
           setTimeout(() => {
             setGameWon(true)
           }, 20)
