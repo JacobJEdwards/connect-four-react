1ac927a89ac102d4384ecc5dda69fc3e4d9b2fed
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-13 20:19:55 +0100

    changed .d.ts file to .ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { ghPages } from 'vite-plugin-gh-pages'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/connect-four-react/",
-  plugins: [react(), ghPages()],
-})
+    plugins: [react(), ghPages()],
+});
